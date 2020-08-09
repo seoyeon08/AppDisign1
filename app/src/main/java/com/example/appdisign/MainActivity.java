843f46456da60a7565a41b326c81dec888ae4414
seoyeon08 <silversun0808@gmail.com>
2020-08-09 22:33:18 +0900

    login

---
+++ app/src/main/java/com/example/appdisign/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.appdisign;
+import android.content.Intent;
+
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appdisign.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yeon_login);
+    }
+
+
+
+
+
+
+
+    //TextInputEditText TextInputEditText_email, TextInputEditText_password;
+    // RelativeLayout RelativeLayout_login;
+
+}
